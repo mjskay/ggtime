ceab11c842846a26bf0609cad7ab6525a00a4634
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-08-25 17:17:29 +1000

    Style geom code

--- R/geom-time-line.R
+++ R/geom-time-line.R
@@ -1,7 +1,15 @@
 #' @export
-geom_time_line <- function(mapping = NULL, data = NULL, stat = "identity",
-                      position = "time_civil", na.rm = FALSE, orientation = NA,
-                      show.legend = NA, inherit.aes = TRUE, ...) {
+geom_time_line <- function(
+  mapping = NULL,
+  data = NULL,
+  stat = "identity",
+  position = "time_civil",
+  na.rm = FALSE,
+  orientation = NA,
+  show.legend = NA,
+  inherit.aes = TRUE,
+  ...
+) {
   layer(
     data = data,
     mapping = mapping,
@@ -22,11 +30,21 @@ geom_time_line <- function(mapping = NULL, data = NULL, stat = "identity",
 #' @usage NULL
 #' @export
 GeomTimeLine <- ggproto(
-  "GeomTimeLine", GeomPath,
+  "GeomTimeLine",
+  GeomPath,
   optional_aes = c("xoffset", "yoffset"),
-  draw_panel = function(self, data, panel_params, coord, arrow = NULL, arrow.fill = NULL,
-                        lineend = "butt", linejoin = "round", linemitre = 10,
-                        na.rm = FALSE) {
+  draw_panel = function(
+    self,
+    data,
+    panel_params,
+    coord,
+    arrow = NULL,
+    arrow.fill = NULL,
+    lineend = "butt",
+    linejoin = "round",
+    linemitre = 10,
+    na.rm = FALSE
+  ) {
     # TODO
     # * Add timezone dashed lines for y-axis offsets
     # * Add gaps for implicit missing values
@@ -35,16 +53,15 @@ GeomTimeLine <- ggproto(
     # If the data is regular across a timezone change (offset) then draw a dashed line
     # Otherwise, there is a gap and there is no dashed timezone line
 
-
     tz_jumps <- c(FALSE, data$xoffset[-1] != data$xoffset[-nrow(data)])
     tz_jumps_i <- which(tz_jumps)
 
     # TODO: Bug doesn't work with multiple tz_jumps
-    data <- data[rep(seq_len(nrow(data)), 1L + tz_jumps),]
+    data <- data[rep(seq_len(nrow(data)), 1L + tz_jumps), ]
     tz_jump_dest <- seq_along(tz_jumps_i) + tz_jumps_i - 1
-    data$x[tz_jump_dest] <- data$x[tz_jump_dest] + diff(data$xoffset[tz_jump_dest - (0:1)])
+    data$x[tz_jump_dest] <- data$x[tz_jump_dest] +
+      diff(data$xoffset[tz_jump_dest - (0:1)])
     data$linetype[tz_jump_dest] <- 2L
-
 
     if (!anyDuplicated(data$group)) {
       cli::cli_inform(c(
@@ -61,7 +78,9 @@ GeomTimeLine <- ggproto(
     # Silently drop lines with less than two points, preserving order
     rows <- stats::ave(seq_len(nrow(munched)), munched$group, FUN = length)
     munched <- munched[rows >= 2, ]
-    if (nrow(munched) < 2) return(ggplot2::zeroGrob())
+    if (nrow(munched) < 2) {
+      return(ggplot2::zeroGrob())
+    }
 
     # Work out grouping variables for grobs (e.g. rm implicit missing)
     # diff(x) == granularity(x) | irregular | timezone jump
@@ -69,15 +88,19 @@ GeomTimeLine <- ggproto(
     n <- nrow(munched)
     group_diff <- munched$group[-1] != munched$group[-n]
     start <- c(TRUE, group_diff)
-    end <-   c(group_diff, TRUE)
+    end <- c(group_diff, TRUE)
 
     munched$fill <- arrow.fill %||% munched$colour
 
     # arrow <- ggplot2::repair_segment_arrow(arrow, munched$group)
 
     grid::segmentsGrob(
-      munched$x[!end], munched$y[!end], munched$x[!start], munched$y[!start],
-      default.units = "native", arrow = arrow,
+      munched$x[!end],
+      munched$y[!end],
+      munched$x[!start],
+      munched$y[!start],
+      default.units = "native",
+      arrow = arrow,
       gp = gg_par(
         col = alpha(munched$colour, munched$alpha)[!end],
         fill = alpha(munched$fill, munched$alpha)[!end],
